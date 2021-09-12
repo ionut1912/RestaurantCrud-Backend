3e6a76f7eb3650c623741a4901174fc0eacd0d5d
ionut1912 <Facultate2018>
2021-09-12 19:08:25 +0300

    Add project files.

---
+++ Controllers/FoodItemController.cs
@@ -0,0 +1,100 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantApi.Models;
+
+namespace RestaurantApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodItemController: ControllerBase
+    {
+        private readonly RestaurantDbContext _context;
+
+        public FoodItemController(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Customer
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FoodItem>>> GetFoodItem()
+        {
+            return await _context.FoodItems.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<FoodItem>> GetFoodItem(int id)
+        {
+            var fooditem = await _context.FoodItems.FindAsync(id);
+
+            if (fooditem == null)
+            {
+                return NotFound();
+            }
+
+            return fooditem;
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFoodItem(int id, FoodItem foodItem)
+        {
+            foodItem.FoodItemId= id;
+
+            _context.Entry(foodItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExistsFoodItem(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/DCandidate
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<FoodItem>> PostFoodItem(FoodItem foodItem)
+        {
+            _context.FoodItems.Add(foodItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFoodItem", new { id =foodItem.FoodItemId }, foodItem);
+        }
+
+        // DELETE: api/DCandidate/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<FoodItem>> DeleteFoodItem(int id)
+        {
+            var foodItem = await _context.FoodItems.FindAsync(id);
+            if (foodItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.FoodItems.Remove(foodItem);
+            await _context.SaveChangesAsync();
+
+            return foodItem;
+        }
+        
+        private bool ExistsFoodItem(int id)
+        {
+            return _context.FoodItems.Any(e => e.FoodItemId == id);
+        }
+    }
+}
